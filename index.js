74b8b5d8fb0fa77a93e9d4341468ee80cb4d4ab7
Cleber Gasparoto <chgasparoto@gmail.com>
2023-11-19 08:38:32 +0100

    feat: implement endpoints

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("node:fs/promises");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", async function (req, res) {
+  let dummyData;
+  for (let i = 0; i < 5000; i++) {
+    dummyData = await fs.readFile("data.json");
+  }
+  const json = JSON.parse(dummyData);
+  res.json({ message: "Success!", data: json });
+});
+
+app.post("/data", async function (req, res) {
+  const data = req.body;
+
+  res.status(201).json({ message: "Received dummy data.", data });
+});
+
+app.listen(3000);
